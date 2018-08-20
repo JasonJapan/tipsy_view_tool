59245fd6745118259a761df17e11113b2b1d9ba7
JasonJapan <jasonsmithdesigner@gmail.com>
2018-08-20 10:13:10 +0900

    Dev tool intial commit

---
+++ lib/tipsy_view_tool.rb
@@ -0,0 +1,6 @@
+require "tipsy_view_tool/version"
+require "tipsy_view_tool/renderer"
+
+module TipsyViewTool
+  # Your code goes here...
+end
---
+++ lib/tipsy_view_tool/version.rb
@@ -0,0 +1,3 @@
+module TipsyViewTool
+  VERSION = "0.1.0"
+end
